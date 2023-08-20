8511032ef4c960430e1dd0ed70ff5d822115930e
bigsaigon333 <likepeppermint@gmail.com>
2023-08-20 10:43:47 +0900

    feat(server): POST /posts api를 구현한다

--- packages/blog-server/src/index.ts
+++ packages/blog-server/src/index.ts
@@ -1,60 +1,50 @@
 // Import the framework and instantiate it
-import Fastify from "fastify";
+import Fastify, { FastifyRequest } from "fastify";
 import Sqlite3 from "sqlite3";
 
 const fastify = Fastify({ logger: true });
 const sqlite3 = Sqlite3.verbose();
-const db = new sqlite3.Database(":memory:");
+const db = new sqlite3.Database("blog-server.db");
 
 db.serialize(() => {
   db.run(`CREATE TABLE IF NOT EXISTS post (
     title TEXT,
     date INTEGER
   )`);
-
-  db.run(
-    "INSERT INTO post (title, date) VALUES ($title, $date)",
-    {
-      $title: `test${Date.now()}`,
-      $date: Date.now(),
-    },
-    function (err) {
-      if (err) {
-        console.error("Error inserting post:", err);
-      } else {
-        console.log("New post inserted with ID:", this.lastID);
-      }
-    }
-  );
-
-  db.run(
-    "INSERT INTO post (title, date) VALUES ($title, $date)",
-    {
-      $title: `test${Date.now()}`,
-      $date: Date.now(),
-    },
-    function (err) {
-      if (err) {
-        console.error("Error inserting post:", err);
-      } else {
-        console.log("New post inserted with ID:", this.lastID);
-      }
-    }
-  );
-
-  db.all("SELECT * FROM post", [], (err, rows) => {
-    if (err) {
-      console.error("Error querying posts:", err);
-    } else {
-      console.log("All posts:", rows);
-    }
-  });
 });
 
 // Declare a route
 fastify.get("/", async function handler(request, reply) {
   return "Hello, world!";
 });
+
+fastify.post(
+  "/posts",
+  (
+    request: FastifyRequest<{ Body: { title: string; date: number } }>,
+    reply
+  ) => {
+    // TODO: title, date runtime validation
+    const { title, date } = request.body;
+
+    db.run(
+      "INSERT INTO post (title, date) VALUES ($title, $date)",
+      { $title: title, $date: date },
+      function (err) {
+        if (err) {
+          console.error("Error inserting post:", err);
+          reply.status(400).send(err.message);
+        } else {
+          console.log("New post inserted with ID:", this.lastID);
+          reply
+            .status(200)
+            .headers({ location: `/posts/${this.lastID}` })
+            .send();
+        }
+      }
+    );
+  }
+);
 
 // Run the server!
 try {

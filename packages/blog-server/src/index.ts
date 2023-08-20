63ab8e28d71dfeadd5b288335fd2ad39cad9bc2e
bigsaigon333 <likepeppermint@gmail.com>
2023-08-20 21:07:12 +0900

    feat: id를 필수 primary key로 설정하고 생성시 unique한 id를 부여한다

--- packages/blog-server/src/index.ts
+++ packages/blog-server/src/index.ts
@@ -1,6 +1,6 @@
-// Import the framework and instantiate it
 import Fastify, { FastifyRequest } from "fastify";
 import Sqlite3 from "sqlite3";
+import { v4 as uuidv4 } from "uuid";
 
 const fastify = Fastify({ logger: true });
 const sqlite3 = Sqlite3.verbose();
@@ -8,14 +8,10 @@ const db = new sqlite3.Database("blog-server.db");
 
 db.serialize(() => {
   db.run(`CREATE TABLE IF NOT EXISTS post (
+    id TEXT PRIMARY KEY NOT NULL,
     title TEXT,
     date INTEGER
   )`);
-});
-
-// Declare a route
-fastify.get("/", async function handler(request, reply) {
-  return "Hello, world!";
 });
 
 fastify.post(
@@ -27,18 +23,20 @@ fastify.post(
     // TODO: title, date runtime validation
     const { title, date } = request.body;
 
+    const id = uuidv4();
+
     db.run(
-      "INSERT INTO post (title, date) VALUES ($title, $date)",
-      { $title: title, $date: date },
+      "INSERT INTO post (title, date, id) VALUES ($title, $date, $id)",
+      { $title: title, $date: date, $id: id },
       function (err) {
         if (err) {
           console.error("Error inserting post:", err);
-          reply.status(400).send(err.message);
+          reply.status(500).send(err.message);
         } else {
-          console.log("New post inserted with ID:", this.lastID);
+          console.log("New post inserted with ID:", id);
           reply
             .status(200)
-            .headers({ location: `/posts/${this.lastID}` })
+            .headers({ location: `/posts/${id}` })
             .send();
         }
       }

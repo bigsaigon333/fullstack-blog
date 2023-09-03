4bf0db2aba4267130b4324b3d025eea5773ee03d
bigsaigon333 <likepeppermint@gmail.com>
2023-09-03 15:57:27 +0900

    feat: 헤더에 홈으로 가는 링크를 추가한다

--- packages/blog-webfront/src/App.tsx
+++ packages/blog-webfront/src/App.tsx
@@ -3,11 +3,11 @@ import { Suspense } from "react";
 import PostsPage from "./pages/PostsPage.js";
 import { globalQueryClient } from "./utils/reactQuery.js";
 
-import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Outlet, Route, Routes } from "react-router-dom";
 import Header from "./components/Header.js";
 
-import "./main.css";
 import Container from "./components/Container.js";
+import "./main.css";
 import PostContentPage from "./pages/PostContentPage.js";
 
 const App = () => {
@@ -19,7 +19,9 @@ const App = () => {
             <Route
               element={
                 <Container>
-                  <Header title="프론트엔드 개발자 김동희입니다" />
+                  <Link to="/">
+                    <Header title="프론트엔드 개발자 김동희입니다" />
+                  </Link>
                   <Outlet />
                 </Container>
               }

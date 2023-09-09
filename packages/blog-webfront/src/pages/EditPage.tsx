90f889a710f716cbd1a6bb28993021ca6426c643
Donghee Kim <dongheekim@toss.im>
2023-09-09 21:20:20 +0900

    feat: ColorModeToggle버튼을 구현한다

--- packages/blog-webfront/src/pages/EditPage.tsx
+++ packages/blog-webfront/src/pages/EditPage.tsx
@@ -1,16 +1,15 @@
-import useColorMode from "../hooks/useColorMode.js";
+import ColorModeToggle from "../components/ColorModeToggle.js";
 
 const EditPage = () => {
-  const [colorMode, _, toggleColorMode] = useColorMode();
-  console.log("🚀 ~ file: EditPage.tsx:5 ~ EditPage ~ a:", colorMode);
-
   return (
     <div>
-      <button onClick={() => toggleColorMode()}>current: {colorMode}</button>
+      <div className="flex justify-end my-4">
+        <ColorModeToggle />
+      </div>
       <form onSubmit={(e) => e.preventDefault()}>
         <div className="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
           <div className="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600">
-            <div className="flex pl-0 space-x-1 sm:pl-2">
+            <div className="flex pl-0 space-x-1">
               <button
                 type="button"
                 className="inline-flex justify-center items-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
@@ -76,7 +75,7 @@ const EditPage = () => {
             <textarea
               id="editor"
               rows={8}
-              className="block w-full px-0 text-sm text-gray-800 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
+              className="block w-full px-0 text-sm text-gray-800 bg-white border-0 dark:bg-gray-800 focus:ring-0 outline-none dark:text-white dark:placeholder-gray-400"
               placeholder="Write an article..."
               required
             ></textarea>

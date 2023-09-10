583543434eaf30e1bf37479423e00d19489e14fb
Donghee Kim <dongheekim@toss.im>
2023-09-10 21:52:36 +0900

    feat: 마크다운에 포함된 링크가 새 탭에서 열리게 한다

--- packages/blog-webfront/src/components/MarkdownRenderer.tsx
+++ packages/blog-webfront/src/components/MarkdownRenderer.tsx
@@ -2,15 +2,25 @@ import DOMPurify from "dompurify";
 import { marked } from "marked";
 import { memo, useMemo } from "react";
 
+marked.use({
+  hooks: {
+    preprocess: (markdown) => markdown,
+    postprocess: (html) => DOMPurify.sanitize(html, { ADD_ATTR: ["target"] }),
+  },
+  renderer: {
+    link: (href, title, text) =>
+      `<a href="${href}" target="_blank" rel="noopener noreferrer" ${
+        title ? `title=${title}` : ""
+      }>${text}</a>`,
+  },
+});
+
 type Props = {
   content: string;
 };
 
 const MarkdownRenderer = ({ content }: Props) => {
-  const html = useMemo(
-    () => DOMPurify.sanitize(marked.parse(content)),
-    [content]
-  );
+  const html = useMemo(() => marked.parse(content), [content]);
 
   return (
     <div

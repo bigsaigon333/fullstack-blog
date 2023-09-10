39bd0a0ab3ecb143fe20392a5b9975dbfc15bb3c
Donghee Kim <dongheekim@toss.im>
2023-09-10 21:47:33 +0900

    fix: draft가 최초에 null으로 초기화되어 RHF defaultValues에 적용되지 않던 문제 해결

--- packages/blog-webfront/src/hooks/useSaveDraft.ts
+++ packages/blog-webfront/src/hooks/useSaveDraft.ts
@@ -2,18 +2,18 @@ import { useAtom } from "jotai";
 
 import { atomWithStorage, useResetAtom } from "jotai/utils";
 import { safeJsonParse } from "../utils/json.js";
+import { useCallback } from "react";
 
 export type Draft = { title: string; content: string };
 
 const draftKeyname = "@@draft";
 const initialDraft = safeJsonParse<Draft>(draftKeyname);
-const draftAtom = atomWithStorage<Draft | null>(draftKeyname, null);
-draftAtom.onMount = (set) => set(initialDraft);
+const draftAtom = atomWithStorage<Draft | null>(draftKeyname, initialDraft);
 
 const useSaveDraft = () => {
   const [draft, setDraft] = useAtom(draftAtom);
 
-  const clearDraft = useResetAtom(draftAtom);
+  const clearDraft = useCallback(() => setDraft(null), [setDraft]);
 
   return [draft, setDraft, clearDraft] as const;
 };

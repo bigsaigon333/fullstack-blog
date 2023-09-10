4db92e42e8915e8ab44742ccbccafc02abea3e04
Donghee Kim <dongheekim@toss.im>
2023-09-10 14:35:18 +0900

    feat: flowbite 를 이용해서 tailwind based design system을 적용한다

--- packages/blog-webfront/src/components/EditForm.tsx
+++ packages/blog-webfront/src/components/EditForm.tsx
@@ -1,5 +1,8 @@
+import { Button, Label, TextInput } from "flowbite-react";
 import { startTransition, useId } from "react";
 import { useForm } from "react-hook-form";
+import { MdAttachFile, MdImage } from "react-icons/md";
+import cx from "classnames";
 
 type EditFormProps = {
   onSubmit?: (data: FormValues) => void;
@@ -23,84 +26,47 @@ export const EditForm = ({ onSubmit }: EditFormProps) => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className="mb-4">
-        <label
-          htmlFor={titleId}
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          제목
-        </label>
-        <input
-          type="text"
+      <div className="flex flex-col mb-4 gap-y-2">
+        <Label htmlFor={titleId}>제목</Label>
+        <TextInput
           id={titleId}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          type="text"
           placeholder="제목을 입력해주세요"
           {...form.register("title", { required: true })}
         />
       </div>
-      <div className="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
-        <div className="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600">
-          <div className="flex pl-0 space-x-1">
-            <button
-              type="button"
-              className="inline-flex justify-center items-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
-            >
-              <svg
-                className="w-4 h-4"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 12 20"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M1 6v8a5 5 0 1 0 10 0V4.5a3.5 3.5 0 1 0-7 0V13a2 2 0 0 0 4 0V6"
-                />
-              </svg>
-              <span className="sr-only">Attach file</span>
-            </button>
-            <button
-              type="button"
-              className="inline-flex justify-center items-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
-            >
-              <svg
-                className="w-4 h-4"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 16 20"
-              >
-                <path d="M8 0a7.992 7.992 0 0 0-6.583 12.535 1 1 0 0 0 .12.183l.12.146c.112.145.227.285.326.4l5.245 6.374a1 1 0 0 0 1.545-.003l5.092-6.205c.206-.222.4-.455.578-.7l.127-.155a.934.934 0 0 0 .122-.192A8.001 8.001 0 0 0 8 0Zm0 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6Z" />
-              </svg>
-              <span className="sr-only">Set location</span>
-            </button>
-            <button
-              type="button"
-              className="inline-flex justify-center items-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
-            >
-              <svg
-                className="w-4 h-4"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 18"
-              >
-                <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
-              </svg>
-              <span className="sr-only">Upload image</span>
-            </button>
-          </div>
+      <div className="w-full mb-4 border border-gray-200 rounded-lg bg-gray-100 dark:bg-gray-700 dark:border-gray-600">
+        <div className="flex items-center px-3 py-2 border-b dark:border-gray-600 gap-x-1">
+          <button
+            className={cx(
+              "p-2",
+              /* disabled style */ "cursor-not-allowed focus:outline-none opacity-50"
+            )}
+            disabled
+          >
+            <MdAttachFile />
+            <span className="sr-only">Attach file</span>
+          </button>
+
+          <button
+            className={cx(
+              "p-2",
+              /* disabled style */ "cursor-not-allowed focus:outline-none opacity-50"
+            )}
+            disabled
+          >
+            <MdImage />
+            <span className="sr-only">Upload image</span>
+          </button>
         </div>
-        <div className="px-4 py-2 bg-white rounded-b-lg dark:bg-gray-800">
+        <div className="px-4 py-2 bg-gray-50 rounded-b-lg dark:bg-gray-800">
           <label htmlFor={editorId} className="sr-only">
             Editor
           </label>
           <textarea
             id={editorId}
             rows={8}
-            className="block w-full px-0 text-sm text-gray-800 bg-white border-0 dark:bg-gray-800 focus:ring-0 outline-none dark:text-white dark:placeholder-gray-400 resize-none"
+            className="block w-full px-0 text-sm bg-inherit text-gray-800 border-0 dark:bg-gray-800 focus:ring-0 outline-none dark:text-white dark:placeholder-gray-400 resize-none"
             placeholder="Write an article..."
             {...form.register("content", {
               required: true,
@@ -116,18 +82,10 @@ export const EditForm = ({ onSubmit }: EditFormProps) => {
       </div>
 
       <div className="flex justify-end gap-x-2">
-        <button
-          type="button"
-          className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-        >
+        <Button color="gray" disabled>
           Save Draft
-        </button>
-        <button
-          type="submit"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-        >
-          Preview post
-        </button>
+        </Button>
+        <Button type="submit">Preview post</Button>
       </div>
     </form>
   );
--- packages/blog-webfront/tailwind.config.js
+++ packages/blog-webfront/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}", "./public/index.html"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./public/index.html",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"), require("flowbite/plugin")],
   darkMode: "class",
 };

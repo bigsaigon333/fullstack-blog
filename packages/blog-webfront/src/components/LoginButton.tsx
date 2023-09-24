d7a35716c102ad0c4dceb61c0882efeae9ae0239
bigsaigon333 <likepeppermint@gmail.com>
2023-09-24 23:24:38 +0900

    feat: kakao 로그인 버튼 클릭시  kakao로 로그인 페이지로 이동한다

--- packages/blog-webfront/src/components/LoginButton.tsx
+++ packages/blog-webfront/src/components/LoginButton.tsx
@@ -1,13 +1,23 @@
+import queryString from "query-string";
 import kakaoLoginButton from "../assets/images/kakao_login_small.png";
+
+const KAKAO_AUTHORIZE_API_ENDPOINT = "https://kauth.kakao.com/oauth/authorize";
+const KAKAO_CLIENT_ID = "38dad1a0f1c2a8f2064197351a79e6ed";
+
+const url = `${KAKAO_AUTHORIZE_API_ENDPOINT}?${queryString.stringify(
+  {
+    response_type: "code",
+    client_id: KAKAO_CLIENT_ID,
+    redirect_uri: "http://localhost:8080/oauth/kakao/authorize",
+  },
+  { sort: false }
+)}`;
 
 export default function LoginButton() {
   return (
-    <button
-      onClick={() => {
-        console.log("onClick");
-      }}
-    >
+    // TODO: 로그인 처리가 다 끝난 후 기존의 url 으로 돌아가야함
+    <a href={url}>
       <img src={kakaoLoginButton} alt="카카오 로그인" />
-    </button>
+    </a>
   );
 }

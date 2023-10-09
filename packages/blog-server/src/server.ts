a86caff64edbec1b730b478bcc7c8fa6f6b11172
bigsaigon333 <likepeppermint@gmail.com>
2023-10-09 22:59:07 +0900

    feat: @fastify/sesion을 적용한다

--- packages/blog-server/src/server.ts
+++ packages/blog-server/src/server.ts
@@ -1,10 +1,27 @@
 import Fastify from "fastify";
 import { Database } from "sqlite3";
+
+import fastifyCookie from "@fastify/cookie";
+import fastifySession from "@fastify/session";
+import crypto from "node:crypto";
+
 import authRoutes from "./routes/authRoutes.js";
 import postRoutes from "./routes/postRoutes.js"; // Adjust the path accordingly
 
 export const startServer = async (db: Database) => {
   const fastify = Fastify();
+
+  const secret = crypto.randomBytes(32).toString("hex");
+
+  fastify.register(fastifyCookie);
+  fastify.register(fastifySession, {
+    secret,
+    cookie: {
+      secure: false, // Set to true for HTTPS
+      maxAge: 86_400_000, // 1 day in milliseconds
+      httpOnly: true,
+    },
+  });
 
   // Register the postRoutes module
   fastify.register(postRoutes, { prefix: "api" });

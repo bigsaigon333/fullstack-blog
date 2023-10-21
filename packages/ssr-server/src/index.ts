07d67c08c9752c502e9e26bb600f120f53ff8bd7
bigsaigon333 <likepeppermint@gmail.com>
2023-10-21 17:50:57 +0900

    feat: /favicon.ico 요청은 404를 보낸다

--- packages/ssr-server/src/index.ts
+++ packages/ssr-server/src/index.ts
@@ -1,20 +1,34 @@
 import Fastify from "fastify";
 import { routeHandler } from "./routes.js";
 
-const fastify = Fastify();
+async function run() {
+  const fastify = Fastify();
 
-try {
-  await Fastify({ logger: true })
-    .route({
-      method: "GET",
-      url: "*",
-      handler: routeHandler,
-    })
-    .listen({ port: 4000 });
+  try {
+    await Fastify({ logger: false })
+      .route({
+        method: "GET",
+        url: "*",
+        preValidation: (request, reply, done) => {
+          if (request.url === "/favicon.ico") {
+            reply.code(404).send();
+            return;
+          }
+          done();
+        },
+        handler: routeHandler,
+      })
+      .setErrorHandler((error, request, reply) => {
+        reply.code(500).send({ ok: false, message: error.message });
+      })
+      .listen({ port: 4000 });
 
-  console.log("Server is running on port 4000");
-} catch (err) {
-  fastify.log.error(err);
+    console.log("Server is running on port 4000");
+  } catch (err) {
+    fastify.log.error(err);
 
-  process.exit(1);
+    process.exit(1);
+  }
 }
+
+run();

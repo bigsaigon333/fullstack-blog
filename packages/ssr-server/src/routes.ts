6ee0d5049b275f94ff06d24e56ad981e28428fbd
bigsaigon333 <likepeppermint@gmail.com>
2023-10-21 22:19:20 +0900

    chore: http response 모듈에 header를 설정한다

--- packages/ssr-server/src/routes.ts
+++ packages/ssr-server/src/routes.ts
@@ -5,7 +5,10 @@ export function routeHandler(request: FastifyRequest, reply: FastifyReply) {
   const { pipe } = ssr.render(request.url, {
     bootstrapScripts: ["/public/main.js"],
     onShellReady: () => {
-      reply.header("content-type", "text/html").header("charset", "utf-8");
+      reply.raw
+        .setHeader("content-type", "text/html")
+        .setHeader("charset", "utf-8");
+
       pipe(reply.raw);
     },
   });

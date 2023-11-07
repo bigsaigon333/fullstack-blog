b6e4e0950e09ec755d5fe94957556f238c3b58b0
bigsaigon333 <likepeppermint@gmail.com>
2023-11-07 22:43:02 +0900

    feat: favicon.ico를 추가한다

--- packages/blog-webfront/src/common.tsx
+++ packages/blog-webfront/src/common.tsx
@@ -11,6 +11,10 @@ export function Html({ children, assetMap }: HtmlProps) {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="icon"
+          href="https://k.kakaocdn.net/14/dn/btsvfBw2B3D/6EJSlhxdZZXbGdPbGAMYTK/o.jpg"
+        />
         {assetMap.css.map((href) => (
           <link key={href} href={href} rel="stylesheet" />
         ))}
